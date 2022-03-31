1c8656a8e6fd1cdcd573f8fba7281e6f64b5ef44
1005244507 <1005244507@L815WD220004.edu.cbe.ab.ca>
2022-03-31 15:04:27 -0600

    3:04

--- Ch3/src/FromMeToYou.java
+++ Ch3/src/FromMeToYou.java
@@ -15,7 +15,11 @@ public class FromMeToYou
 
     public static void main(String args[])
     {
-       
+        System.out.println("From: Mr. Wilson\r\n"
+        		+ "Address: Acer Computer, Lab 314\r\n"
+        		+ "Date: November 2, 2020\r\n"
+        		+ "To: Kearmouy Heng\r\n"
+        		+ "Message: Help! I'm trapped inside a computer!");
        
     }
 
