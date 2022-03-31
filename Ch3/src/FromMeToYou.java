db2f1eaa03b0a1683d118c7c884eb63210901e23
1005244507 <1005244507@L815WD220004.edu.cbe.ab.ca>
2022-03-31 14:58:09 -0600

    2:58

---
+++ Ch3/src/FromMeToYou.java
@@ -0,0 +1,28 @@
+/*
+
+Program: Assignment1.java          Date: 3-31-2022
+
+
+Author: Kearmouy Heng
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+
+public class FromMeToYou 
+{
+
+    public static void main(String args[])
+    {
+       
+       
+    }
+
+}
+/* Screen Dump
+ 
+
+
+ 
+ */
---
+++ Ch3/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+  public static void main(String[] args) {
+	  // This is a comment so only I can see this
+    System.out.println("Hello World");
+  }
+}
