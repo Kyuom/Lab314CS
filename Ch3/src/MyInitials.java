b351784a3046a201d5b3d41fa5d7db0ee0c3051b
1005244507 <1005244507@L815WD220004.edu.cbe.ab.ca>
2022-03-31 15:25:46 -0600

    3:25

---
+++ Ch3/src/MyInitials.java
@@ -0,0 +1,41 @@
+/*
+
+Program: Assignment1.java          Date: 3-31-2022
+
+
+Author: Kearmouy Heng
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+
+public class MyInitials {
+
+	public static void main(String[] args) {
+		System.out.println("|       /\r\n"
+				+ "|     /\r\n"
+				+ "|     /r\n"
+				+ "|    /r\n"
+				+ "|  /\r\n"
+				+ "| /\r\n"
+				+ "| /r\n"
+				+ "|/r\n"
+				+ "|\\r\n"
+				+ "|\\r\n"
+				+ "|  \\r\n"
+				+ "|    \\r\n"
+				+ "|     \\r\n"
+				+ "|      \\r\n"
+				+ "|       \\r\n"
+				+ "|        \\r\n");
+
+	}
+
+}
+/* Screen Dump
+
+
+
+
+ */
