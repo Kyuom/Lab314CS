ad3b06c96520bb0b319792a702ebf646ef028adf
1005244507 <1005244507@L815WD220004.edu.cbe.ab.ca>
2022-04-01 11:03:13 -0600

    11:03

---
+++ Ch3/src/BingoCard.java
@@ -0,0 +1,25 @@
+/*
+
+Program: Assignment1.java          Date: 3-31-2022
+
+
+Author: Kearmouy Heng
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+
+public class BingoCard {
+
+	public static void main(String[] args) {
+		System.out.println("B     I     N     G     O\r\n"
+				+ "8     17    34    52    64\r\n"
+				+ "13    19    42    56    68\r\n"
+				+ "5     23   FREE   60    75\r\n"
+				+ "9     28    38    51    70\r\n"
+				+ "2     30    43    57    69\r\n");
+
+	}
+
+}
