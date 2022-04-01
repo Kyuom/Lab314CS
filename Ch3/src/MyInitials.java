cf8b2a9a18b4301b77da497fea65fed1064a9cc3
1005244507 <1005244507@L815WD220004.edu.cbe.ab.ca>
2022-04-01 10:52:39 -0600

    10:52

--- Ch3/src/MyInitials.java
+++ Ch3/src/MyInitials.java
@@ -13,22 +13,21 @@ Course: Computer Science 10
 public class MyInitials {
 
 	public static void main(String[] args) {
-		System.out.println("|       /\r\n"
-				+ "|     /\r\n"
-				+ "|     /r\n"
-				+ "|    /r\n"
-				+ "|  /\r\n"
-				+ "| /\r\n"
-				+ "| /r\n"
-				+ "|/r\n"
-				+ "|\\r\n"
-				+ "|\\r\n"
-				+ "|  \\r\n"
-				+ "|    \\r\n"
-				+ "|     \\r\n"
-				+ "|      \\r\n"
-				+ "|       \\r\n"
-				+ "|        \\r\n");
+		System.out.println("*      *     *      *\r\n"
+				+ "*     *      *      *\r\n"
+				+ "*    *       *      *\r\n"
+				+ "*   *        *      *\r\n"
+				+ "*  *         *      *\r\n"
+				+ "* *          *      *\r\n"
+				+ "**           *      *\r\n"
+				+ "*            ********\r\n"
+				+ "**           *      *\r\n"
+				+ "* *          *      *\r\n"
+				+ "*  *         *      *\r\n"
+				+ "*   *        *      *\r\n"
+				+ "*    *       *      *\r\n"
+				+ "*     *      *      *\r\n"
+				+ "*      *  .  *      *\r\n");
 
 	}
 
