e9a87f175e53d7f84399bd0ed47d78f4861d3b4b
1005244507 <1005244507@L815WD220004.edu.cbe.ab.ca>
2022-04-01 11:32:28 -0600

    11:32

---
+++ Ch3/src/PizzaCost.java
@@ -0,0 +1,29 @@
+/*
+
+Program: Assignment1.java          Date: 3-31-2022
+
+
+Author: Kearmouy Heng
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+
+public class PizzaCost {
+
+	public static void main(String[] args) {
+		System.out.println ("Enter the diameter of the pizza in inches:\r\n"
+				+ "The cost of making the pizza is:");
+		
+		// The labor cost of the pizza
+		int x = 7.5
+ 		x += 5;
+		
+		// The rent cost of the pizza
+		
+		
+		// The material cost of the pizza
+	}
+
+}
