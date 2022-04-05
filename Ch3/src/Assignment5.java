d4ef014c48cd708fd038d09c6be1c7bebf7fb911
1005244507 <1005244507@L815WD220004.edu.cbe.ab.ca>
2022-04-05 15:28:38 -0600

    3:28

---
+++ Ch3/src/Assignment5.java
@@ -0,0 +1,32 @@
+/*
+
+Program: Assignment5.java          Date: 04-05-2022
+
+
+Author: Kearmouy Heng
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+public class Assignment5 {
+
+	public static void main(String[] args) {
+
+int answer1 = 79 + 3 * (4 + 82 - 68) - 7 + 19;
+int answer2 = (179 + 21 + 10) / 7 + 181;
+int answer3 = 10389 * 56 * 11 + 2246;
+
+System.out.println("79 + 3 * (4 + 82 - 68) - 7 + 19 = " +answer1);
+System.out.println("(179 + 21 + 10) / 7 + 181 = " +answer2);
+System.out.println("10389 * 56 * 11 + 2246 = " +answer3);
+
+	}
+
+}
+/* Screen Dump
+
+
+
+
+ */
