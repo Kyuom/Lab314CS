1df4522236979024e8284c0006162ef95b8fc4bf
1005244507 <1005244507@L815WD220004.edu.cbe.ab.ca>
2022-04-11 13:57:44 -0600

    1:57

---
+++ Credit5/src/EvenOrOdd.java
@@ -0,0 +1,29 @@
+/*
+
+Program: Assignment10.java          Date: 04-11-2022
+
+
+Author: Kearmouy Heng
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+import java.util.Scanner;
+
+public class EvenOrOdd {
+
+	public static void main(String[] args) {
+		
+		Scanner myObj = new Scanner (System.in);
+		
+		System.out.print("Enter an integer: ");
+		int integer = myObj.nextInt();
+		
+		System.out.print("The integer "
+				+integer
+				+" is even");
+
+	}
+
+}
