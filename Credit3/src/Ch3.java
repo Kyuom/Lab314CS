0b50fbcc11a4ce4b16a20a16120345a6a6095d56
1005244507 <1005244507@L815WD220004.edu.cbe.ab.ca>
2022-04-12 15:07:14 -0600

    3:07

---
+++ Credit3/src/Ch3.java
@@ -0,0 +1,26 @@
+/*
+
+Program: Ch3.java          Date: 03-31-2022
+
+
+Author: Kearmouy Heng
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+
+public class Ch3 {
+
+	public static void main(String[] args) {
+		// Kearmouy
+
+	}
+
+}
+/* Screen Dump
+
+
+
+
+ */
