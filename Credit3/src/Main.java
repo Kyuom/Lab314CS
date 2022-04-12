32b54a7ee0bbe21c75d75dd1cda18e8b2d9ed03e
1005244507 <1005244507@L815WD220004.edu.cbe.ab.ca>
2022-04-12 15:01:44 -0600

    3:01

---
+++ Credit3/src/Main.java
@@ -0,0 +1,24 @@
+/*
+
+Program: Main.java          Date: 03-31-2022
+
+
+Author: Kearmouy Heng
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+
+public class Main {
+  public static void main(String[] args) {
+	  // This is a comment so only I can see this
+    System.out.println("Hello World");
+  }
+}
+/* Screen Dump
+
+Hello World.
+
+
+ */
