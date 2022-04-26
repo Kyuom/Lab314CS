958c4624b1468fdd245d4636957f22c45c68fda1
1005244507 <1005244507@L815WD220004.edu.cbe.ab.ca>
2022-04-26 15:19:18 -0600

    3:19

---
+++ Credit5/src/Assignment15.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/*
+
+Program: Assignment15.java          Date: 04-26-2022
+
+
+Author: Kearmouy Heng
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+public class Assignment15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
