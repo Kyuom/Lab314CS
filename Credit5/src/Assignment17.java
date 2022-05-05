a532a21a18f03d8a280aae1bd7724d7ecf2506bf
1005244507 <1005244507@L815WD220004.edu.cbe.ab.ca>
2022-05-05 14:59:57 -0600

    2:59

--- Credit5/src/Assignment17.java
+++ Credit5/src/Assignment17.java
@@ -15,55 +15,53 @@ public class Assignment17 {
 
 	public static void main(String[] args) {
 		Scanner myObj = new Scanner (System.in);
-		do {
+	 	do {
 		System.out.print("Enter your grade average point (GPA) or -1 to quit the program: " + "\r\n"); //Prompt user to enter their GPA
 		double ui = myObj.nextDouble(); //User input
 		{
-		while (ui < -1)
+		while (ui < -1) //If the user input is less than -1
 		{
 		    System.out.println("Please re-enter GPA from 0.0-4.0 or -1 to quit program: ");
-		    ui = myObj.nextDouble();
+		    ui = myObj.nextDouble(); // User input
 		}
-		while (ui > 4.0)
+		while (ui > 4.0) //If the user input is greater than 4
 		{
 		    System.out.println("Please re-enter GPA from 0.0-4.0 or -1 to quit program: ");
-		    ui = myObj.nextDouble();
+		    ui = myObj.nextDouble(); //User input
 		}
 		
-		if (ui == -1)
+		if (ui == -1) //If user input is -1 print "Program quit"
 		{
-			System.out.println("Program Quit");
+			System.out.println("Program quit");
 		}
-		
-		while (ui != -1)
-		{
-		if (ui >= 3.8 && ui <= 4.0)
+		else if (ui >= 3.8 && ui <= 4.0) //If user input is in between 3.8-4.0 print "Summa cum laude"
 		{
 			System.out.println("Summa cum laude");
 		}
-		else if (ui >= 3.65 && ui < 3.8)
+		else if (ui >= 3.65 && ui < 3.8) //If user input is in between 3.65-3.8 print 3.65-3.8 print "Magna cum laude"
 		{
 			System.out.println("Magna cum laude");
 		}
-		else if (ui >= 3.5 && ui < 3.65)
+		else if (ui >= 3.5 && ui < 3.65) //If user input is between 3.5-3.65 print "Cum laude"
 		{
 			System.out.println("Cum laude");
 		}
-		else
+		else //If user input is not any of the above print "Sorry, at this you did not qualify for an honors distinction."
 		{
 			System.out.println("Sorry, at this you did not qualify for an honors distinction.");
 		}
-		break;
-		}
 		
 	
-	System.out.println("Would you like to enter another GPA? (Y/N)");
-	String a = myObj.next();
-		}while(a == "Y"); 
+		System.out.println("Would you like to enter another GPA? (Y/N)"); //Prompt the user to enter another GPA
+		String a = myObj.nextLine(); //User answer input
+	
+		}while(!a.equals("Y") && !a.equals("y")); //If user input is not "Y" or "y"
 		
 	System.out.println("Thank you for using our program, have a great day!");
 		
 		}
 	}
+}
+
 	
 		
