80b7cd0c8dd64c49e71896059fbddd9892099722
1005244507 <1005244507@L815WD220004.edu.cbe.ab.ca>
2022-05-10 15:15:06 -0600

    3:15

--- Credit5/src/FinalProject.java
+++ Credit5/src/FinalProject.java
@@ -28,61 +28,46 @@ public class FinalProject {
 			P = myObj.nextLine();
 		}
 		
-		System.out.print("\r\n" + "Username is: " + UN + "\r\n" + "Password is: " + P);
+		System.out.print("\r\n" + "Username is: " + UN + "\r\n" + "Password is: " + P + "\r\n");
 		
 		String a;
 	 	do {
-		System.out.print("\r\n" + "Enter your grades from 0.0-100 to calculate your average or -1 to quit the program: " + "\r\n"); //Prompt user to enter their grades or -1 to quit
+		System.out.println("\r\n" + "Enter 5 grades from 0.0-100 to calculate your average: "); //Prompt user to enter their grades or -1 to quit
+		System.out.print("Grade 1: ");
 		int G1 = myObj.nextInt(); //User input
+		System.out.print("Grade 2: ");
 		int G2 = myObj.nextInt(); //User input
+		System.out.print("Grade 3: ");
 		int G3 = myObj.nextInt(); //User input
+		System.out.print("Grade 4: ");
 		int G4 = myObj.nextInt(); //User input
+		System.out.print("Grade 5: ");
+		int G5 = myObj.nextInt(); //User input
 		System.out.print("\r\n");
 		
-		while (G1 < -1 && G2 < -1 && G3 < -1 && G4 < -1) //If the user input is less than -1
-		{
-		    System.out.println("Please re-enter grades from 0.0-100 or -1 to quit program: ");
-		    G1 = myObj.nextInt(); //User input
-		    G2 = myObj.nextInt(); //User input
-			G3 = myObj.nextInt(); //User input
-			G4 = myObj.nextInt(); //User input
-		    System.out.print("\r\n");
-		}
-		while (G1 > 100 && G2 > 100 && G3 > 100 && G4 > 100) //If the user input is greater than 4
-		{
-		    System.out.println("Please re-enter grades from 0.0-100 or -1 to quit program: ");
-		    G1 = myObj.nextInt(); //User input
-		    G2 = myObj.nextInt(); //User input
-			G3 = myObj.nextInt(); //User input
-			G4 = myObj.nextInt(); //User input
-		    System.out.print("\r\n");
-		}
-		
-		if (G1 == -1 && G2 == -1 && G3 == -1 && G4 == -1) //If user input is -1 print "Program quit"
-		{
-			System.out.println("Program quit" + "\r\n");
-		}
-		
-		double A = (G1 + G2 + G3 + G4) / 4;
-		System.out.print("Your average is: " + A);
+		double A = (G1 + G2 + G3 + G4 + G5) / 5;
+		System.out.print("Your average is: " + A + "\r\n");
 		    
 		
 		if (A >= 80 && A <= 100) //If average is 80-100
 		{
-			System.out.println("Honours Student" + "\r\n");
+			System.out.println("You are a Honours student" + "\r\n");
 		}
 		else if (A >= 50 && A < 79) //If average is 50-79
 		{
-			System.out.println("Passing student" + "\r\n");
+			System.out.println("You are a Passing student" + "\r\n");
 		}
-		else (A >= 0 && A < 49) //If average is 0-49
+		else if (A >= 0 && A < 49) //If average is 0-49
 		{
-			System.out.println("Failing Student" + "\r\n");
+			System.out.println("You are a Failing student" + "\r\n");
+		}
+		else
+		{
+			System.out.println("Invalid grade" + "\r\n");
 		}
 	
 		System.out.println("Would you like to enter another grade? (Y/N)"); //Prompt the user to enter another grade
 		a = myObj.nextLine() + myObj.nextLine(); //User answer input
-		System.out.print("\r\n");
 		
 	 	}while (!a.equals("N") && !a.equals("n")); //If user input is not "N" or "n" loop
 	
